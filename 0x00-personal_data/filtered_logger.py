69417bf0b8ff816b7ffcdd8eb19b0ebdfe2b7f2d
umuhozalionel <muhozalionel@gmail.com>
2024-11-08 21:55:41 +0000

    Initial commit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,11 +4,12 @@ Module for filtering sensitive data in log messages
 """
 
 import re
+import logging
 from typing import List
 
 
 def filter_datum(
-    fields: List[str], redaction: str, message: str, separator: str
+        fields: List[str], redaction: str, message: str, separator: str
 ) -> str:
     """
     Obfuscates log messages by replacing values of specified fields with
@@ -33,3 +34,28 @@ def filter_datum(
             message
         )
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Filters values in incoming log records using filter_datum.
+        """
+        original_message = super(RedactingFormatter, self).format(record)
+        return filter_datum(
+            self.fields,
+            self.REDACTION,
+            original_message,
+            self.SEPARATOR
+        )
