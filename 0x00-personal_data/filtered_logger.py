6e3f126c12a8f67ee8e7f3e4efb29d3157a6450c
umuhozalionel <muhozalionel@gmail.com>
2024-11-08 21:38:39 +0000

    Initial commit

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Module for filtering sensitive data in log messages
+"""
+
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """
+    Obfuscates log messages by replacing values of specified fields with a redaction string.
+
+    Args:
+        fields (List[str]): List of strings representing all fields to obfuscate.
+        redaction (str): String representing by what the field will be obfuscated.
+        message (str): String representing the log line.
+        separator (str): String representing by which character is separating all fields in the log line.
+
+    Returns:
+        str: Obfuscated log message.
+    """
+    for field in fields:
+        message = re.sub(f"{field}=[^{separator}]*", f"{field}={redaction}", message)
+    return message
+
