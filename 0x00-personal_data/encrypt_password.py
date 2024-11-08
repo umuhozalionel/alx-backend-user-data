ee6636d3a0d8d872125deefc1160548bf04eb6e9
umuhozalionel <muhozalionel@gmail.com>
2024-11-08 22:23:07 +0000

    Initial commit

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Module for hashing passwords
+"""
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Hashes a password using bcrypt.
+
+    Args:
+        password (str): The password to hash.
+
+    Returns:
+        bytes: The hashed password as a byte string.
+    """
+    # Generate a salt
+    salt = bcrypt.gensalt()
+
+    # Hash the password with the generated salt
+    hashed_password = bcrypt.hashpw(password.encode(), salt)
+
+    return hashed_password
--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -3,12 +3,8 @@
 Main file
 """
 
-import logging
-import re
+hash_password = __import__('encrypt_password').hash_password
 
-RedactingFormatter = __import__('filtered_logger').RedactingFormatter
-
-message = "name=Bob;email=bob@dylan.com;ssn=000-123-0000;password=bobby2019;"
-log_record = logging.LogRecord("my_logger", logging.INFO, None, None, message, None, None)
-formatter = RedactingFormatter(fields=("email", "ssn", "password"))
-print(formatter.format(log_record))
+password = "MyAmazingPassw0rd"
+print(hash_password(password))
+print(hash_password(password))
