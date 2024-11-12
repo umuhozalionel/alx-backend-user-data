b594a1ea51892c7b4a31d9823f9505089c38326d
umuhozalionel <muhozalionel@gmail.com>
2024-11-12 19:39:59 +0000

    Initial commit

--- 0x01-Basic_authentication/index.py
+++ 0x01-Basic_authentication/index.py
@@ -23,7 +23,7 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
-@app_views.route('/api/v1/unauthorized', methods=['GET'])
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized():
     """ GET /api/v1/unauthorized
     Raises:
