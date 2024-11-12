5357db3fcba85065036bb05c0ac30d71e1788917
umuhozalionel <muhozalionel@gmail.com>
2024-11-12 22:08:19 +0000

    Initial commit

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -21,11 +21,10 @@ def unauthorized(error) -> str:
     """ Unauthorized handler """
     response = jsonify({"error": "Unauthorized"})
     response.status_code = 401
-    response.headers["Content-Length"] = str(len(response.data))
-    response.headers["Connection"] = "close"
+    response.direct_passthrough = False  # Ensure data calculation
     return response
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port, threaded=False)
+    app.run(host=host, port=port)
