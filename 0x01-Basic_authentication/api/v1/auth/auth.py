92c10959363c393eed9f16f480339a041b214484
umuhozalionel <muhozalionel@gmail.com>
2024-11-12 22:48:52 +0000

    Initial commit

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,17 +1,12 @@
-# api/v1/auth/auth.py
-
 from flask import request
 from typing import List, TypeVar
 
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Determine if authentication is required """
         return False
 
     def authorization_header(self, request=None) -> str:
-        """ Get the authorization header from the request """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ Get the current user """
         return None
