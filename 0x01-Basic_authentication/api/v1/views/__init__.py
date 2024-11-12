4bed541f0c06e1946659c5347f7f593f97e89aca
umuhozalionel <muhozalionel@gmail.com>
2024-11-12 21:49:57 +0000

    Initial commit

--- 0x01-Basic_authentication/api/v1/views/__init__.py
+++ 0x01-Basic_authentication/api/v1/views/__init__.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-from models.user import User
 from api.v1.views.blueprint import app_views
-
 import api.v1.views.index
 import api.v1.views.users  # If this file exists
+from models.user import User
 
 User.load_from_file()
